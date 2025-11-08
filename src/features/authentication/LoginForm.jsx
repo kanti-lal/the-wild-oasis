cbbff66648ddf6a24d7ca41d1b935d2cb785a031
kanti-lal <kanti.ylocation@gmail.com>
2025-11-09 00:16:45 +0530

    fix: minor improvement

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("kanti@gmail.com");
-  const [password, setPassword] = useState("kanti123");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const { login, isPending } = useLogin();
 
